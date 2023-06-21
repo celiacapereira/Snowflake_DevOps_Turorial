63eb1483392a3bf877a46ff3e4028be79bbf8833
Célia Pereira <90316668+celiacapereira@users.noreply.github.com>
2023-06-21 10:18:35 +0100

    Update orders.sql

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -1,4 +1,4 @@
-WITH ORDERS AS (
+/*WITH ORDERS AS (
 SELECT ORDER_ID,
     USER_ID,
     EVAL_SET,
@@ -16,7 +16,7 @@ FINAL AS (
     ORDER_HOUR_OF_DAY,
     DAYS_SINCE_PRIOR_ORDER
     FROM ORDERS
-)/*
+)
 select ORDER_ID,
     USER_ID,
     EVAL_SET,
@@ -25,5 +25,14 @@ select ORDER_ID,
     DAYS_SINCE_PRIOR_ORDER from final
 
 */ 
+WITH ORDERS AS (
+SELECT * FROM {{ ref('stg_orders') }}
+),
+    
+FINAL AS (
+    SELECT
+  *
+    FROM ORDERS
+)
 select 
   * from final
